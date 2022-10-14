6d40856843bb1481d7cf3b3164c0f9b6a58935ab
williammunnich <williammunnich@pm.me>
2022-10-14 17:09:44 -0500

    started chat bot for rck paper scissors

--- spc.py
+++ spc.py
@@ -1,36 +1,37 @@
 #prompt user for rock paper scissors
-#random fucntion 0-2 
-#   0=rock
-#   1=paper
-#   2=scissors
+
 import random as rn
 
-options = ('rock', 'paper', 'scissors')
-
-rpc = input("Choose either 'rock', 'paper', or 'scissors': ").lower()
-
-while rpc != 'rock' and rpc != 'paper' and rpc != 'scissors':
-    print("Not a valid input try again")
-    #print(rpc)
-    rpc = input("Choose either 'rock', 'paper', or 'scissors': ").lower()
+def RockPaperScissors(choice):
     
-rand = rn.choice(options)
-print(rand)
+    options = ('rock', 'paper', 'scissors')
 
-if rpc == rand:
-    print("Tie")
-elif rpc == "rock":
-    if rand == "paper":
-        print("you lose")
-    else:
-        print('you won')
-elif rpc == "paper":
-    if rand == "scissors":
-        print("you lose")
-    else:
-        print('you won')
-elif rpc == "scissors":
-    if rand == "rock":
-        print("you lose")
-    else:
-        print('you won')                                                                                                  
+    
+
+    '''while rpc != 'rock' and rpc != 'paper' and rpc != 'scissors':
+        print("Not a valid input try again")
+        #print(rpc)
+     '''   
+        
+    rand = rn.choice(options)
+    print(rand)
+
+    if choice == rand:
+        return "Tie"
+    elif choice == "rock":
+        if rand == "paper":
+            return "you lose"
+        else:
+            return 'you won'
+    elif choice == "paper":
+        if rand == "scissors":
+            return "you lose"
+        else:
+            return 'you won'
+    elif choice == "scissors":
+        if rand == "rock":
+            return "you lose"
+        else:
+            return 'you won'                                                                                                  
+rpc = input("Choose either 'rock', 'paper', or 'scissors': ").lower()
+print(RockPaperScissors(rpc))
