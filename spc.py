91bc238e737e06f89c32be265b6ea2e2c2013c4e
williammunnich <williammunnich@pm.me>
2022-10-14 20:58:45 -0500

    test
    Works but not super well

---
+++ spc.py
@@ -0,0 +1,37 @@
+#prompt user for rock paper scissors
+
+import random as rn
+
+def RockPaperScissors(choice):
+    
+    options = ('rock', 'paper', 'scissors')
+
+    
+
+    '''while rpc != 'rock' and rpc != 'paper' and rpc != 'scissors':
+        print("Not a valid input try again")
+        #print(rpc)
+     '''   
+        
+    rand = rn.choice(options)
+    print(rand)
+
+    if choice == rand:
+        return "you both chose "  + choice + ". it's a tie"
+    elif choice == "rock":
+        if rand == "paper":
+            return "you chose "  + choice + ". The bot chose " + rand + '. you lost'
+        else:
+            return "you chose "  + choice + ". The bot chose " + rand + '. you won'
+    elif choice == "paper":
+        if rand == "scissors":
+            return "you chose "  + choice + ". The bot chose " + rand + '. you lost'
+        else:
+            return "you chose "  + choice + ". The bot chose " + rand + '. you won'
+    elif choice == "scissors":
+        if rand == "rock":
+            return "you chose "  + choice + ". The bot chose " + rand + '. you lost'
+        else:
+            return "you chose "  + choice + ". The bot chose " + rand + '. you won'                                          
+#rpc = input("Choose either 'rock', 'paper', or 'scissors': ").lower()
+#print(RockPaperScissors(rpc))
